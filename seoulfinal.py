4a85c13e274e33fef8f96dfd97f65587f4a4b0e1
Shin Dae kyeon(Ain) <62682548+AIN108@users.noreply.github.com>
2026-09-02 17:04:41 +0900

    fix: move KMA API key to environment variable

--- seoulfinal.py
+++ seoulfinal.py
@@ -1,58 +1,61 @@
-import RPi.GPIO as GPIO
-import requests
-import xmltodict
+import os
 import time
 
-def get_weather_data():
-    url = 'http://apis.data.go.kr/1360000/VilageFcstMsgService/getLandFcst'
-    params = {
-        'serviceKey': 'H/+1O7x6RzhSsqBmKv5jh9zdkccdPiWXz7GNB1Lkwkrwo4L7EFhthFyPTt27WDbp6LAeSXo9I8fgfxYZuOH2VQ==',
-        'pageNo': '1',
-        'numOfRows': '1',
-        'dataType': 'XML',
-        'regId': '11B10101'
-    }
-    response = requests.get(url, params=params)
-    data = xmltodict.parse(response.content)
-    return data['response']['body']['items']['item']
+import requests
+import RPi.GPIO as GPIO
+import xmltodict
 
-# GPIO 설정
+
+def get_service_key():
+    service_key = os.getenv("KMA_SERVICE_KEY")
+    if not service_key:
+        raise RuntimeError(
+            "KMA_SERVICE_KEY 환경변수가 설정되지 않았습니다. "
+            "공공데이터포털에서 새 키를 발급한 뒤 환경변수로 설정하세요."
+        )
+    return service_key
+
+
+def get_weather_data():
+    url = "https://apis.data.go.kr/1360000/VilageFcstMsgService/getLandFcst"
+    params = {
+        "serviceKey": get_service_key(),
+        "pageNo": "1",
+        "numOfRows": "1",
+        "dataType": "XML",
+        "regId": "11B10101",
+    }
+    response = requests.get(url, params=params, timeout=15)
+    response.raise_for_status()
+    data = xmltodict.parse(response.content)
+    return data["response"]["body"]["items"]["item"]
+
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup([7, 8, 11, 12], GPIO.OUT, initial=GPIO.LOW)
 
 try:
     while True:
-        # 날씨 데이터 가져오기
         item = get_weather_data()
 
-        # 날씨 데이터 추출
-        skystate = item['wfCd']  # 날씨 코드
-        rain = item['rnYn']      # 강수 여부
+        skystate = item["wfCd"]
+        rain = item["rnYn"]
 
-        # GPIO에 신호 보내기
-        if skystate == 'DB01':  # 맑음
+        GPIO.output([7, 8, 11], GPIO.LOW)
+        if skystate == "DB01":
             GPIO.output(7, GPIO.HIGH)
-        elif skystate == 'DB03':  # 구름 많음
+        elif skystate == "DB03":
             GPIO.output(8, GPIO.HIGH)
-        elif skystate == 'DB04':  # 흐림
+        elif skystate == "DB04":
             GPIO.output(11, GPIO.HIGH)
 
-        if rain == '0':  # 강수 없음
-            GPIO.output(12, GPIO.LOW)
-        else:  # 강수 있음
-            GPIO.output(12, GPIO.HIGH)
+        GPIO.output(12, GPIO.LOW if rain == "0" else GPIO.HIGH)
 
         print(f"Sky State: {skystate}, Rain: {rain}")
-
-        # 6시간 대기
         time.sleep(10)
 
 except KeyboardInterrupt:
     print("프로그램이 중단되었습니다.")
-
 finally:
     GPIO.cleanup()
-
-
-
